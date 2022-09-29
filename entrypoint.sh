e21e8721036eb64aa9983d70d2ff8ab36f620d7a
Brian Chen <brianchen8990@gmail.com>
2022-09-29 14:06:21 +0200

    Revert "fix: passing related github env variables to inner docker file"
    This reverts commit 8590ac4b0ae2e384669ebfc72e89c676fdb7598d.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,4 @@ echo "creating docker image with flutter version: $FLUTTER_VERSION"
 
 # here we can make the construction of the image as customizable as we need
 # and if we need parameterizable values it is a matter of sending them as inputs
-docker build -t action_app \
-	--build-arg flutter_version="$FLUTTER_VERSION" \
-	GITHUB_WORKSPACE="$GITHUB_WORKSPACE" \
-	GITHUB_REPOSITORY="$GITHUB_REPOSITORY" \
-	GITHUB_SHA="$GITHUB_SHA" \
-	. && docker run action_app
+docker build -t action_app --build-arg flutter_version="$FLUTTER_VERSION" . && docker run action_app
